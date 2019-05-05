f3be495bed3f5b972fec964504894f076a2da1b6
Oleg Barmin <obarmin@cisco.com>
2019-05-05 22:20:22 +0300

    Make 'PaymentReason' entity readonly form client side.

--- contractors/server/src/main/java/com/university/contractors/controller/PaymentReasonController.java
+++ contractors/server/src/main/java/com/university/contractors/controller/PaymentReasonController.java
@@ -3,7 +3,15 @@ package com.university.contractors.controller;
 import com.university.contractors.config.Endpoints;
 import com.university.contractors.model.PaymentReason;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PaymentReasonController extends AbstractCrudControllerBase<Long, PaymentReason> {
@@ -27,18 +35,18 @@ public class PaymentReasonController extends AbstractCrudControllerBase<Long, Pa
     @Override
     @PostMapping(path = Endpoints.PAYMENT_REASONS)
     public PaymentReason create(@RequestBody PaymentReason entityToCreate) {
-        return super.create(entityToCreate);
+        throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @Override
     @PutMapping(path = Endpoints.PAYMENT_REASON_BY_ID)
     public PaymentReason update(@PathVariable Long id, @RequestBody PaymentReason entityToUpdateWith) {
-        return super.update(id, entityToUpdateWith);
+        throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @Override
     @DeleteMapping(path = Endpoints.PAYMENT_REASON_BY_ID)
     public void delete(@PathVariable Long id) {
-        super.delete(id);
+        throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
