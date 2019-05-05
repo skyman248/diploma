f9808d8ca3d8f2f5a9cd9937f29a3ce070d62153
Oleg Barmin <obarmin@cisco.com>
2019-05-05 20:21:06 +0300

    Make 'date' in PaymentRecordDto formatted in millis.

--- contractors/server/src/main/java/com/university/contractors/controller/dto/PaymentRecordDto.java
+++ contractors/server/src/main/java/com/university/contractors/controller/dto/PaymentRecordDto.java
@@ -3,10 +3,12 @@ package com.university.contractors.controller.dto;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.university.contractors.model.PaymentCurrent;
 
 public class PaymentRecordDto {
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
     private Date date;
     private List<PaymentCurrent> paymentsList;
 
