d28c9414a135099fab0f436b760e6c0ee25f58c1
Oleg Barmin <obarmin@cisco.com>
2019-05-05 19:08:53 +0300

    Add 'PaymentTableRepository'

---
+++ contractors/server/src/main/java/com/university/contractors/repository/PaymentTableRepository.java
@@ -0,0 +1,13 @@
+package com.university.contractors.repository;
+
+import com.university.contractors.model.PaymentCurrent;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface PaymentTableRepository extends CrudRepository<PaymentCurrent, Long> {
+
+    @Query(value = "SELECT * FROM Contractors.dbo.payment_current WHERE ref_contract = :contractId ORDER BY pay_date", nativeQuery = true)
+    Iterable<PaymentCurrent> getByContractIdOrderByPayDate(@Param("contractId") Long contractId);
+
+}
